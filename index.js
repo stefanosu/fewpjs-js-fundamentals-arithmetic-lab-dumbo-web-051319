aaf06f5451ebf43aa5c2d452172616f3754c1790
Stefanos <stefanosu422@gmail.com>
2019-07-06 22:11:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+let num1 = 31 
+let num2 = 2 
+let multiply = num1 * num2 
+
+
+let random = Math.floor(Math.random() * 50 ) +2
+
+let mod = 16% 6
+
+let max = Math.max(10, 15, 20)
